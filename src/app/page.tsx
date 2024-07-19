d57213f0acb7f0f96c267dd18e08f20ae72a6715
Amit Biswas <amit.142biswas@gmail.com>
2024-07-19 23:01:50 +0530

    bfgf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+const NAVIGATION_ITEMS = [
+  {
+    title: 'Home',
+  }
+]
 
 const page = () => {
   return (
-    <div>
+    <div className="w-full relative h-full flex items-center justify-center">
+      <div className="max-w-screen-lg w-full h-full flex relative">
+        {/* {side navigation} */}
+        <section className="fixed w-72 flex flex-col">
+
+        </section>
+        <main> </main>
+        <section> </section>
+      </div>
       
     </div>
   )
