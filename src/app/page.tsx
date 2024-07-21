08cf85d8e88ddeb8be1a69693196d6b7a2a51532
Amit Biswas <amit.142biswas@gmail.com>
2024-07-21 20:45:35 +0530

    dihn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,60 @@
-import Link from "next/link"
-import { BiHomeCircle } from "react-icons/bi"
-import { FiUser } from "react-icons/fi";
-import { IoBookmarkOutline } from "react-icons/io5";
-import { GoBell } from "react-icons/go"
-import { HiOutlineEnvelope } from "react-icons/hi2"
-import { RiTwitterXFill } from "react-icons/ri"
-import { RiHashtag } from "react-icons/ri";
-import { FaUser } from "react-icons/fa";
+import LeftSidebar from "@/components/LeftSidebar";
 import { BsThreeDots } from "react-icons/bs";
-
-const NAVIGATION_ITEMS = [
-  {
-    title: 'Home',
-    icon: BiHomeCircle
-  },
-  {
-    title: 'Explore',
-    icon: RiHashtag
-  },
-  {
-    title: 'Notification',
-    icon: GoBell 
-  },
-  {
-    title: 'Messages',
-    icon: HiOutlineEnvelope 
-  },
-  {
-    title: 'Bookmarks',
-    icon: IoBookmarkOutline
-  },
-  {
-    title: 'Profile',
-    icon: FiUser 
-  }
-]
 
 const page = () => {
   return (
     <div className="w-full relative h-full flex items-center justify-center bg-black">
       <div className="max-w-screen-xl w-full h-full flex relative">
         {/* {side navigation} */}
-        <section className="fixed w-72 flex text-lg flex-col h-screen space-y-4 my-4 pl-40">
-          <Link href={'/'} className='p-2 text-2xl'>
-            <RiTwitterXFill className='h-8 w-8' />
-          </Link>
-          {
-            NAVIGATION_ITEMS.map((item) => (
-              <Link 
-                className="hover:bg-white/50 transition duration-200 flex items-center justify-start w-fit space-x-2 rounded-3xl p-2.5" 
-                href={`/${item.title.toLowerCase()}`} key={item.title}>
-                <div><item.icon className='mr-6 h-7 w-7' /></div>
-                <div>{item.title}</div>
-              </Link>
-            ))
-          }
-          <button className='w-60 rounded-full bg-primary px-6 py-4 text-lg text-center hover:bg-blue-400'>
-            Post
-          </button>
-          <button className='w-60 rounded-full flex items-center space-x-2 px-6 py-4 text-lg text-center hover:bg-slate-700'>
-            <div className="rounded-full bg-primary w-10 h-10 text-center"><FaUser className="mt-2 mb-2 ml-3" /></div>
-            <div className="items-center">
-              <div className="text-sm font-bold">Username</div>
-              <div className='text-xs font-semibold text-slate-400'>login email</div>
-            </div>
-            <div><BsThreeDots className="h-6 w-6 ml-10" /></div>
-          </button>
-        </section>
+        <LeftSidebar />
         {/* <main>Home Timeline</main> */}
+        <main className="ml-[425px] p-3 flex w-[600px] h-full min-h-screen flex-col border-r-[0.5px] border-l-[0.5px] border-gray-900">
+          <h1 className="text-2xl font-bold ">Home</h1>
+          <div className="border-t-[0.5px] border-b-[0.5px] border-gray-700 h-40  relative p-4">
+            <div className="w-10 h-10 bg-slate-900 rounded-full flex-none"></div>
+            <div className="flex flex-col">
+              <div className="border-b-[0.5px] ">
+                <input 
+                type="text" 
+                placeholder="What's happening?" 
+                className="w-full h-full underline-offset-0 bg-transparent outline-none border-none border-gray-700"
+                />
+              </div>
+              <div className="w-full flex justify-between items-center">
+                <div></div>
+                <div>
+                  <button className="rounded-full w-full m-4 bg-primary px-4 py-2 text-lg text-center hover:bg-opacity-70 transition duration-200">
+                    Post 
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col'>
+            {
+              Array.from({length: 5}).map((_, i) => (
+                <div className="border-t-[0.5px] border-b-[0.5px] px-4">
+                  <div>
+                    <div className="w-10 h-10 bg-slate-200 rounded-full" />
+                  </div>
+                  <div>
+                    <div className="flex items-center space-x-1">
+                      <div>Amit Biswas</div>
+                      <div>@amitbiswas</div>
+                      <div>
+                        <BsThreeDots />
+                      </div>
+                      <div>1 hour ago</div>
+                    </div>
+                    <div className="text-white text-sm">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos natus, velit libero delectus, molestias cum, fugiat optio architecto tenetur provident obcaecati nobis veritatis eveniet adipisci dicta. Corrupti id nobis maiores ad sunt dolorum nemo quod necessitatibus error quaerat laudantium temporibus esse ipsa expedita molestias, ea reprehenderit fugiat eaque veniam voluptas?</div>
+                    <div></div>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </main>
         {/* <section>right section</section> */}
       </div>
       
